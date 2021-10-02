19a5b35f24d003f8572f4f02f2311faa62872ca7
Souhail Merroun <souhail.merroun1996@gmail.com>
2021-10-03 00:57:54 +0200

    Layer logic

--- popup.js
+++ popup.js
@@ -5,3 +5,22 @@ chrome.storage.sync.get("color", ({color}) => {
     changeColor.style.backgroundColor = color
     
 })
+
+// On click 
+changeColor.addEventListener("click", async () => {
+
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: setPageBackgroundColor
+    });
+
+});
+
+// Function
+function setPageBackgroundColor() {
+    chrome.storage.sync.get("color", ({ color}) => {
+        document.body.style.backgroundColor = color;
+    });
+}
