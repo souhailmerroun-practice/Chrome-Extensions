94299c66ed29e382b9cf83e582977b47b0d7ace7
Souhail Merroun <souhail.merroun1996@gmail.com>
2021-10-03 01:14:01 +0200

    Rich notifications API

--- background.js
+++ background.js
@@ -4,4 +4,12 @@ chrome.runtime.onInstalled.addListener(() => {
     console.log("Hello world!");
 
     chrome.storage.sync.set({ color });
+
+    chrome.notifications.create("id", {
+        type: "basic",
+        title: "ETH Price Alert",
+        message: "ETH has reached $3400",
+        iconUrl: "https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png"
+    }, function () {});
 });
+
